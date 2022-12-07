078c0947b922cc74bb2fd577b6084e9965bbbae6
rafaelvf <reviterif@gmail.com>
2022-12-07 16:56:56 +0000

    update size of image

--- pages/index.tsx
+++ pages/index.tsx
@@ -301,8 +301,8 @@ export default function Home() {
             <Image
               src="/design.png"
               alt="s"
-              width={100}
-              height={100}
+              width={150}
+              height={150}
               className="mx-auto absolute top-0 left-0"
             />
           </div>
